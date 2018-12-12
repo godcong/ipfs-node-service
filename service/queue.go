59ef2001bc04677435fcb0385c4684dd41c7fdf8
godcong <jumbycc@163.com>
2018-12-12 15:41:34 +0800

    add Start

--- service/queue.go
+++ service/queue.go
@@ -3,65 +3,49 @@ package service
 import (
 	"github.com/dgraph-io/badger"
 	"sync"
+	"time"
 )
 
-// Streamer ...
-type Streamer struct {
+// HandleFunc ...
+type HandleFunc func(name, key string) error
+
+// StreamInfo ...
+type StreamInfo struct {
 	key      string
 	fileName string
-	status   int
-	step     int
 }
 
 // NewStreamer ...
-func NewStreamer(key string, fileName string) *Streamer {
-	return &Streamer{key: key, fileName: fileName}
-}
-
-// Status ...
-func (s *Streamer) Status() int {
-	return s.status
-}
-
-// SetStatus ...
-func (s *Streamer) SetStatus(status int) {
-	s.status = status
-}
-
-// Step ...
-func (s *Streamer) Step() int {
-	return s.step
-}
-
-// SetStep ...
-func (s *Streamer) SetStep(step int) {
-	s.step = step
+func NewStreamer(key string, fileName string) *StreamInfo {
+	return &StreamInfo{key: key, fileName: fileName}
 }
 
 // FileName ...
-func (s *Streamer) FileName() string {
+func (s *StreamInfo) FileName() string {
 	return s.fileName
 }
 
 // SetFileName ...
-func (s *Streamer) SetFileName(fileName string) {
+func (s *StreamInfo) SetFileName(fileName string) {
 	s.fileName = fileName
 }
 
 // Key ...
-func (s *Streamer) Key() string {
+func (s *StreamInfo) Key() string {
 	return s.key
 }
 
 // SetKey ...
-func (s *Streamer) SetKey(key string) {
+func (s *StreamInfo) SetKey(key string) {
 	s.key = key
 }
 
 // Queue ...
 type Queue struct {
-	db    *badger.DB
-	queue sync.Pool
+	flag   bool
+	db     *badger.DB
+	queue  sync.Pool
+	handle HandleFunc
 }
 
 // NewQueue ...
@@ -102,14 +86,78 @@ func (q *Queue) DB() *badger.DB {
 }
 
 // Push ...
-func (q *Queue) Push(v *Streamer) {
+func (q *Queue) Push(v *StreamInfo) {
 	q.queue.Put(v)
 }
 
 // Pop ...
-func (q *Queue) Pop() *Streamer {
+func (q *Queue) Pop() *StreamInfo {
 	if v := q.queue.Get(); v != nil {
-		return v.(*Streamer)
+		return v.(*StreamInfo)
 	}
 	return nil
+}
+
+// Handle ...
+func (q *Queue) Handle() HandleFunc {
+	return q.handle
+}
+
+// SetHandle ...
+func (q *Queue) SetHandle(handle HandleFunc) {
+	q.handle = handle
+}
+
+// Run ...
+func (q *Queue) Start(process int) {
+	q.flag = false
+	//run with a new go channel
+	go func() {
+		threads := make(chan int, process)
+
+		for i := 0; i < process; i++ {
+			for {
+				if q.flag {
+					close(threads)
+					return
+				}
+				if s := q.Pop(); s != nil {
+					go transfer(threads, s)
+					break
+				}
+				time.Sleep(5 * time.Second)
+			}
+		}
+
+		for {
+			select {
+			case _ = <-threads:
+				for {
+					if q.flag {
+						close(threads)
+						return
+					}
+					if s := q.Pop(); s != nil {
+						go transfer(threads, s)
+						break
+					}
+					time.Sleep(5 * time.Second)
+				}
+			default:
+				if q.flag {
+					close(threads)
+					return
+				}
+			}
+		}
+	}()
+}
+
+// Stop ...
+func (q *Queue) Stop() {
+	q.flag = true
+}
+
+func transfer(chanints chan<- int, info *StreamInfo) {
+
 }
